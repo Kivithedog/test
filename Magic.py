3dbb35be0b4559fed9043e5c2e491da85c456683
Kivithedog <victory27@nycstudents.net>
2024-12-16 08:45:02 -0500

    d

--- Magic.py
+++ Magic.py
@@ -8,5 +8,10 @@ with open('./Magic.json', 'r',  encoding='utf-8-sig') as file:
 
 #print all cards that have '10E' in printings
 
+for card in data:
+    if set in card:
+        if set == '10e':
+            print(card['name'])
+
 # Print all cards where the card is restricted in the Paupercommander format
 
